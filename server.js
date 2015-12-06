6b2b72ddec6f1fc458fbd3f91f3a81310eeea36e
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-12-05 20:18:14 -0800

    Added route for sending html file

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var taskRouter = require(__dirname + '/routes');
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/task');
 
 app.use(taskRouter);
+app.use(express.static(__dirname + '/public'));
 
 app.listen((process.env.PORT || 3000), function() {
   console.log('Server has started on port ' + (process.env.PORT || 3000));
