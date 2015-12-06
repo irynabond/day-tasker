50213f1b44f6b64cbb1e2b4aa38bfa4ed433c052
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-12-05 19:11:41 -0800

    Added add button

--- public/script.js
+++ public/script.js
@@ -1,12 +1,6 @@
-function buildTasks(tasks, placeholder) {
-	var tasksPlaceholder = document.createElement("div");
-	for(var i = 0; i<tasks.length; ++i) {
-		buildTask(tasks[i], tasksPlaceholder);
-	}
-	placeholder.appendChild(tasksPlaceholder);
-}
+var taskId = 1;
 
-function buildTask(task, placeholder) {
+function buildTask(task, $placeholder) {
 	var $taskPlaceholder = $("<div></div>");
 	var $done = $("<input type='checkbox'></input>");
 	var $text = $("<span></span>");
@@ -14,10 +8,38 @@ function buildTask(task, placeholder) {
 	$text.text(task.text);
 	
 	$taskPlaceholder.append($done).append($text);
-	placeholder.appendChild($taskPlaceholder[0]);
-}
+	$taskPlaceholder.attr('data-task-id', task.id);
+	$placeholder.append($taskPlaceholder);
+};
+
+function buildTasks(tasks, $placeholder) {
+	if($placeholder) {
+		$placeholder[0].innerHTML = "";
+	} else {
+		$placeholder = $("<div></div>");
+	}
+	for(var i = 0; i<tasks.length; ++i) {
+		buildTask(tasks[i], $placeholder);
+	}
+	return $placeholder;
+};
+
+
+
+function addTask(tasks, text) {
+	var task = {id: taskId++, text: text, checked: false};
+	tasks.push(task);
+	return task;
+};
 
 $(function(){
-		var someTasks = [{id: 1, text: 'Task', done: false}, {id: 2, text: 'Some', done: true}];
-		buildTasks(someTasks, $('#tasks')[0]);
+		var someTasks = [{id: taskId++, text: 'Task', done: false}, {id: taskId++, text: 'Some', done: true}];
+		var $addButton = $('<button>Add button</button>');
+		var $tasksPlaceholder = buildTasks(someTasks);
+		$addButton.click(function(){
+			var task = addTask(someTasks, prompt("Enter new task"));
+			buildTask(task, $tasksPlaceholder);
+		});
+		$('#tasks').append($addButton);
+		$('#tasks').append($tasksPlaceholder);
 });
