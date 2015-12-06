03dc1a41bd61e7aa8b4f024a64d73c5942e35ec5
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-12-05 18:51:09 -0800

    Added very basic list of tasks rendering

--- public/script.js
+++ public/script.js
@@ -1,3 +1,23 @@
+function buildTasks(tasks, placeholder) {
+	var tasksPlaceholder = document.createElement("div");
+	for(var i = 0; i<tasks.length; ++i) {
+		buildTask(tasks[i], tasksPlaceholder);
+	}
+	placeholder.appendChild(tasksPlaceholder);
+}
+
+function buildTask(task, placeholder) {
+	var $taskPlaceholder = $("<div></div>");
+	var $done = $("<input type='checkbox'></input>");
+	var $text = $("<span></span>");
+	$done.prop('checked', task.done);
+	$text.text(task.text);
+	
+	$taskPlaceholder.append($done).append($text);
+	placeholder.appendChild($taskPlaceholder[0]);
+}
+
 $(function(){
-	console.log('onload');
+		var someTasks = [{id: 1, text: 'Task', done: false}, {id: 2, text: 'Some', done: true}];
+		buildTasks(someTasks, $('#tasks')[0]);
 });
